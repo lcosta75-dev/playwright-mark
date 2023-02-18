cf6982486ec7cef70e3c909fb6f5d9e321cc9889
Lais Costa <lcosta.developer@gmail.com>
2023-02-18 13:27:34 -0300

    trabalhando com massa de teste fixa - request API

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,9 +1,14 @@
 import { test, expect } from '@playwright/test'
 
-test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
-    await page.goto('http://192.168.0.136:3000')
+test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+
+    const taskName = 'Ler um livro de TypeScript'
+
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+
+    await page.goto('http://localhost:3000')
 
     const inputTaskName = page.locator('input[class*=InputNewTask]')
-    await inputTaskName.fill('Ler um livro de TypeScript')
+    await inputTaskName.fill(taskName)
     await page.click('css=button >> text=Create')
 })
