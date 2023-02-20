70bc25bd50e06d144b27238bac92da5c912ea325
Lais Costa <lcosta.developer@gmail.com>
2023-02-20 10:03:33 -0300

    implementado campo obrigatório convertendo elemento do browser em objeto HTML

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -1,11 +1,13 @@
-import { Page, expect } from '@playwright/test'
+import { Page, expect, Locator } from '@playwright/test'
 import { TaskModel } from '../../../fixtures/task.model'
 
 export class TasksPage {
     readonly page: Page
+    readonly inputTaskName: Locator
 
     constructor(page: Page){
         this.page = page
+        this.inputTaskName = page.locator('input[class*=InputNewTask]')
     }
 
     async go(){
@@ -13,9 +15,7 @@ export class TasksPage {
     }
 
     async create(task: TaskModel){
-        const inputTaskName = this.page.locator('input[class*=InputNewTask]')
-        await inputTaskName.fill(task.name)
-
+        await this.inputTaskName.fill(task.name)
         await this.page.click('css=button >> text=Create')
     }
 
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './support/pages/tasks'
@@ -10,7 +10,7 @@ test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
         is_done: false
     }
 
-    await deleteTaskByHelper(request, task.name)    
+    await deleteTaskByHelper(request, task.name)
 
     const tasksPage: TasksPage = new TasksPage(page)
 
@@ -33,4 +33,19 @@ test('não deve permitir tarefa duplicada', async ({ page, request }) => {
     await page.goto('http://localhost:3000')
     await tasksPage.create(task)
     await tasksPage.alertHaveText('Task already exists!')
+})
+
+test('campo obrigatório', async ({ page }) => {
+    const task: TaskModel = {
+        name: '',
+        is_done: false
+    }
+    const tasksPage: TasksPage = new TasksPage(page)
+
+    await page.goto('http://localhost:3000')
+    await tasksPage.create(task)
+
+    //convertendo elemento para um objeto HTML
+    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+    expect(validationMessage).toEqual('This is a required field')
 })
